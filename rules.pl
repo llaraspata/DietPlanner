2afab81d21d24a397e8850be93158a5443a025e3
Lucrezia Laraspata <l.laraspata3@studenti.uniba.it>
2023-09-03 19:34:49 +0200

    added nutrient distribution rules

--- rules.pl
+++ rules.pl
@@ -9,12 +9,16 @@
 % Nutrient distribution
 % ---------
 % 1. Protein should make up about 10-15% of total calories.
+rule(1, daily_diet_total_nutrient_percentage(DailyDiet, protein, TotalPercentage), and([TotalPercentage > 9, TotalPercentage < 16])).
 
 % 2. Carbohydrates should make up about 45-65% of total calories.
+rule(2, daily_diet_total_nutrient_percentage(DailyDiet, carbs, TotalPercentage), and([TotalPercentage > 44, TotalPercentage < 46])).
 
 % 3. Lipids should make up about 20-35% of total calories.
+rule(3, daily_diet_total_nutrient_percentage(DailyDiet, lipids, TotalPercentage), and([TotalPercentage > 19, TotalPercentage < 36])).
 
 % 4. Dietary Fibers should be 25-30 g per day.
+rule(4, daily_diet_total_nutrient_grams(DailyDiet, dietary_fiber, TotalGrams), and([TotalGrams > 24, TotalGrams < 31])).
 
 
 

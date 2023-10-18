8786494682e90d82efeea65e3ec046d1e1310734
Lucrezia Laraspata <l.laraspata3@studenti.uniba.it>
2023-10-18 12:13:02 +0200

    added question explanations

--- src/prolog/diet_type/questionnaire.pl
+++ src/prolog/diet_type/questionnaire.pl
@@ -68,81 +68,81 @@ attribute_value(dietplanner, body_goal, name, 'Body goal').
 % ---------
 question_instance(dietplanner, question, q1).
 attribute_value(dietplanner, q1, text, 'Do you have any dietary restrictions to report?').
-attribute_value(dietplanner, q1, asked_why, 'TODO').
-attribute_value(dietplanner, q1, mandatory, TODO).
-attribute_value(dietplanner, q1, importance, TODO).
+attribute_value(dietplanner, q1, asked_why, 'It is useful to assess an individual\'s dietary needs and preferences, as it allows for the identification of any specific dietary restrictions, allergies, or cultural/religious preferences that might influence the type of diet that can be recommended effectively. This question ensures that the suggested diet plan is personalized and suitable for the individual\'s unique requirements and constraints.').
+attribute_value(dietplanner, q1, mandatory, 1).
+attribute_value(dietplanner, q1, importance, 1).
 
 question_instance(dietplanner, question, q2).
 attribute_value(dietplanner, q2, text, 'Do you eat meat?').
-attribute_value(dietplanner, q2, asked_why, 'TODO').
-attribute_value(dietplanner, q2, mandatory, TODO).
-attribute_value(dietplanner, q2, importance, TODO).
+attribute_value(dietplanner, q2, asked_why, 'It is crucial in a questionnaire aimed at deducing the proper diet type to suggest because it helps determine whether the individual follows a vegetarian, vegan, or omnivorous diet, which is fundamental for tailoring dietary recommendations and ensuring dietary compatibility with the person\'s existing dietary choices and preferences.').
+attribute_value(dietplanner, q2, mandatory, 1).
+attribute_value(dietplanner, q2, importance, 1).
 
 question_instance(dietplanner, question, q3).
 attribute_value(dietplanner, q3, text, 'Do you eat fish and seafood?').
-attribute_value(dietplanner, q3, asked_why, 'TODO').
-attribute_value(dietplanner, q3, mandatory, TODO).
-attribute_value(dietplanner, q3, importance, TODO).
+attribute_value(dietplanner, q3, asked_why, 'It helps establish whether an individual includes or excludes fish and seafood in their diet, which is essential for determining if they follow a vegetarian, vegan, or other diet, and enables personalized diet recommendations that align with their dietary habits.').
+attribute_value(dietplanner, q3, mandatory, 1).
+attribute_value(dietplanner, q3, importance, 1).
 
 question_instance(dietplanner, question, q4).
 attribute_value(dietplanner, q4, text, 'Do you eat animal-derived foods?').
-attribute_value(dietplanner, q4, asked_why, 'TODO').
-attribute_value(dietplanner, q4, mandatory, TODO).
-attribute_value(dietplanner, q4, importance, TODO).
+attribute_value(dietplanner, q4, asked_why, 'It is intended to deduce the proper diet type to suggest because it provides insights into whether an individual consumes any animal products, which is fundamental for categorizing their diet as vegetarian, or vegan.').
+attribute_value(dietplanner, q4, mandatory, 1).
+attribute_value(dietplanner, q4, importance, 1).
 
 question_instance(dietplanner, question, q5).
 attribute_value(dietplanner, q5, text, 'Do you have any particular medical conditions or chronic illnesses to report?').
-attribute_value(dietplanner, q5, asked_why, 'TODO').
-attribute_value(dietplanner, q5, mandatory, TODO).
-attribute_value(dietplanner, q5, importance, TODO).
+attribute_value(dietplanner, q5, asked_why, 'It is crucial in a questionnaire aimed at deducing the proper diet type to suggest because it helps identify any health-related dietary restrictions or requirements, ensuring that dietary recommendations take into account the individual\'s medical conditions, or specific health needs for a safe and effective dietary plan.').
+attribute_value(dietplanner, q5, mandatory, 1).
+attribute_value(dietplanner, q5, importance, 1).
 
 question_instance(dietplanner, question, q6).
 attribute_value(dietplanner, q6, text, 'Are you diabetic?').
-attribute_value(dietplanner, q6, asked_why, 'TODO').
-attribute_value(dietplanner, q6, mandatory, TODO).
-attribute_value(dietplanner, q6, importance, TODO).
+attribute_value(dietplanner, q6, asked_why, 'It is essential in a questionnaire designed to deduce the proper diet type to suggest because it directly determines whether the individual has diabetes, a condition that significantly influences dietary choices.').
+attribute_value(dietplanner, q6, mandatory, 1).
+attribute_value(dietplanner, q6, importance, 1).
 
 question_instance(dietplanner, question, q7).
 attribute_value(dietplanner, q7, text, 'Do you suffer from high blood pressure?').
-attribute_value(dietplanner, q7, asked_why, 'TODO').
-attribute_value(dietplanner, q7, mandatory, TODO).
-attribute_value(dietplanner, q7, importance, TODO).
+attribute_value(dietplanner, q7, asked_why, 'It is important in a questionnaire aimed at deducing the proper diet type to suggest because it helps identify a specific medical condition that requires dietary considerations. Knowing if someone has high blood pressure is crucial for recommending a diet that can support blood pressure management and overall cardiovascular health.').
+attribute_value(dietplanner, q7, mandatory, 1).
+attribute_value(dietplanner, q7, importance, 1).
 
 question_instance(dietplanner, question, q8).
 attribute_value(dietplanner, q8, text, 'Do you have high cholesterol?').
-attribute_value(dietplanner, q8, asked_why, 'TODO').
-attribute_value(dietplanner, q8, mandatory, TODO).
-attribute_value(dietplanner, q8, importance, TODO).
+attribute_value(dietplanner, q8, asked_why, 'It is significant in a questionnaire designed to deduce the proper diet type to suggest because it alerts to a specific health condition that has a direct impact on dietary recommendations. Understanding whether an individual has high cholesterol, together with high blood pressure, is essential for tailoring a diet plan that can assist in managing cholesterol levels and reducing cardiovascular risk.').
+attribute_value(dietplanner, q8, mandatory, 1).
+attribute_value(dietplanner, q8, importance, 1).
 
 question_instance(dietplanner, question, q9).
 attribute_value(dietplanner, q9, text, 'Do you suffer from gastro-intestinal disorders?').
-attribute_value(dietplanner, q9, asked_why, 'TODO').
-attribute_value(dietplanner, q9, mandatory, TODO).
-attribute_value(dietplanner, q9, importance, TODO).
+attribute_value(dietplanner, q9, asked_why, 'It is crucial in a questionnaire aimed at deducing the proper diet type to suggest because it helps identify specific gastrointestinal health issues that require dietary modifications. Recognizing the presence of such disorders is essential for customizing a diet plan that can alleviate symptoms and support digestive health.').
+attribute_value(dietplanner, q9, mandatory, 1).
+attribute_value(dietplanner, q9, importance, 1).
 
 question_instance(dietplanner, question, q10).
 attribute_value(dietplanner, q10, text, 'Do you suffer from kidney problems?').
-attribute_value(dietplanner, q10, asked_why, 'TODO').
-attribute_value(dietplanner, q10, mandatory, TODO).
-attribute_value(dietplanner, q10, importance, TODO).
+attribute_value(dietplanner, q10, asked_why, 'It is pivotal in a questionnaire designed to deduce the proper diet type to suggest because it indicates a specific medical condition that significantly impacts dietary choices.').
+attribute_value(dietplanner, q10, mandatory, 1).
+attribute_value(dietplanner, q10, importance, 1).
 
 question_instance(dietplanner, question, q11).
 attribute_value(dietplanner, q11, text, 'Do you simply want to reach your healthy weight?').
-attribute_value(dietplanner, q11, asked_why, 'TODO').
-attribute_value(dietplanner, q11, mandatory, TODO).
-attribute_value(dietplanner, q11, importance, TODO).
+attribute_value(dietplanner, q11, asked_why, 'It is relevant in a questionnaire aimed at deducing the proper diet type to suggest because it helps determine the primary goal of the individual, which is essential for tailoring a diet plan that focuses on weight management and overall well-being.').
+attribute_value(dietplanner, q11, mandatory, 1).
+attribute_value(dietplanner, q11, importance, 1).
 
 question_instance(dietplanner, question, q12).
 attribute_value(dietplanner, q12, text, 'Do you want to increase your muscle mass?').
-attribute_value(dietplanner, q12, asked_why, 'TODO').
-attribute_value(dietplanner, q12, mandatory, TODO).
-attribute_value(dietplanner, q12, importance, TODO).
+attribute_value(dietplanner, q12, asked_why, 'It is important in a questionnaire aimed at deducing the proper diet type to suggest because it signals a specific fitness goal, allowing for the customization of a diet plan that emphasizes higher protein intake and appropriate nutrient balance to support muscle growth and athletic performance.').
+attribute_value(dietplanner, q12, mandatory, 1).
+attribute_value(dietplanner, q12, importance, 1).
 
 question_instance(dietplanner, question, q13).
 attribute_value(dietplanner, q13, text, 'Do you want to reduce your body fat percentage?').
-attribute_value(dietplanner, q13, asked_why, 'TODO').
-attribute_value(dietplanner, q13, mandatory, TODO).
-attribute_value(dietplanner, q13, importance, TODO).
+attribute_value(dietplanner, q13, asked_why, 'It is crucial in a questionnaire aimed at deducing the proper diet type to suggest because it indicates a specific weight management goal, enabling the tailoring of a diet plan that emphasizes calorie control to support the individual in achieving their desired body composition.').
+attribute_value(dietplanner, q13, mandatory, 1).
+attribute_value(dietplanner, q13, importance, 1).
 
 
 % ---------

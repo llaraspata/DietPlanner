92185a12a28e51913f88cf9ff78c82dcbea02b1b
Lucrezia Laraspata <l.laraspata3@studenti.uniba.it>
2023-11-06 20:20:03 +0100

    added demo user for diet type inference

---
+++ src/prolog/diet_type/demo.pl
@@ -0,0 +1,11 @@
+fact(has_dietary_restrictions(mario_rossi)).
+fact(do_not_eat(mario_rossi, meat)).
+fact(do_not_eat(mario_rossi, fish_seafood)).
+fact(do_not_eat(mario_rossi, animal_derived)).
+fact(has_medical_issues(mario_rossi)).
+fact(has(mario_rossi, diabetes)).
+fact(has(mario_rossi, high_blood_pressure)).
+fact(has(mario_rossi, high_cholesterol)).
+fact(has(mario_rossi, gastrointestinal_disorders)).
+fact(has(mario_rossi, kidney_problems)).
+fact(wants_to(mario_rossi, reach_healthy_weight)).
